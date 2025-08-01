7aa7a8a3815630db5ee5c7f15aa7d56e2882c90e
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-08-01 15:13:00 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -28,7 +28,7 @@ async function pingCommand(sock, chatId, message) {
         const uptimeInSeconds = process.uptime();
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
-        const botInfo = `🔸 *𝙹𝚄𝙽𝙴 𝙼𝙳 𝚜𝚙𝚎𝚎𝚍:* ${ping} ms`.trim();
+        const botInfo = `🔸 *𝙹𝚄𝙽𝙴* 𝚜𝚙𝚎𝚎𝚍: ${ping} ms`.trim();
 
         // Reply to the original message with the bot info
         await sock.sendMessage(chatId, { text: botInfo},{ quoted: message });
