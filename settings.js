927ad5b20d5aa0de4bff26cd63b0d9cd424544ef
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-08-12 18:16:59 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
   packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
-  author: 'supreme',
+  author: 'SUPREME',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
   botOwner: 'Supreme', // Your name
   ownerNumber: '254798570132', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.2.0",
+  version: "1.2.1",
 };
 
 module.exports = settings;
