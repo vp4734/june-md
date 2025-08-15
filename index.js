54f2d927aaa96e249afa5f80932a99c2268c1003
Tennor-modz <giddycraz@gmail.com>
2025-08-15 12:19:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function downloadSessionData() {
         return console.log(color(`Session id not found at SESSION_ID!\nCreds.json not found at session folder!\n\nWait to enter your number`, 'red'));
       }
 
-      const base64Data = global.SESSION_ID.split("trashcore~")[1];
+      const base64Data = global.SESSION_ID.split("JUNE:~")[1];
       
       const sessionData = Buffer.from(base64Data, 'base64');
       
