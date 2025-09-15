e5aab76165eae1d05bb1497163ae17d3ea0bbce7
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-09-15 14:35:47 +0300

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -117,7 +117,7 @@ async function facebookCommand(sock, chatId, message) {
         await sock.sendMessage(chatId, {
             video: { url: tempFile },
             mimetype: "video/mp4",
-            caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧"
+            caption: "Downloaded ✅"
         }, { quoted: message });
 
         // Clean up temp file
