634b0d76da4a92fa9072d68df935dc709ce94268
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-09-30 20:53:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
   // Session Configuration
   sessionId: process.env.SESSION_ID || "Caseyrhodes~YOUR_SESSION_ID_HERE",
-  packname: 'JINX-XMD',
+  packname: 'JUNE-MD',
   author: '‎',
-  botName: "JINX-XMD",
-  botOwner: 'Professor',
-  ownerNumber: '254112192119',
+  botName: "JUNE-MD",
+  botOwner: '',
+  ownerNumber: '',
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
