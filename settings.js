68b435dc2b17b37f78ac4f7fa9cdad85ae91b5f5
SuprEmE2 <166421298+Vinpink2@users.noreply.github.com>
2025-10-01 01:29:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,8 +9,8 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "2.5.1",
-  updateZipUrl: "https://github.com/vinpink2/june-private-repohide/archive/refs/heads/main.zip",
+  version: "2.5.9",
+  //updateZipUrl: "",
 };
 
 module.exports = settings;
