f75f0aab8a73ec2540d233c4066ddd0ae93efc70
caseyrhodes <caseywebsthedonn25@gmail.com>
2025-09-30 16:45:57 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴛᴇᴄʜ

---
+++ settings.js
@@ -0,0 +1,21 @@
+//session-id
+
+global.SESSION_ID = process.env.SESSION_ID || ''
+
+
+const settings = {
+  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
+  author: '‎',
+  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
+  botOwner: 'supreme', // Your name
+  ownerNumber: '254798570132', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.5.1",
+  updateZipUrl: "https://github.com/vinpink2/june-private-repohide/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
